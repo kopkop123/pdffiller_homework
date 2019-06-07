f61c0dbdb39148994f18e1aba0aec142e1f03527
Maxim Piven <shpax1@gmail.com>
2019-06-07 11:27:45 +0300

    added console.log

--- js/answers.js
+++ js/answers.js
@@ -1,6 +1,8 @@
 function saveAnswer(question, answerText) {
   const { correctOption } = question;
 
+  console.log(answerText);
+
   const isCorrect = correctOption === null ? null : correctOption === answerText;
   return { question, answerText, isCorrect }
 }
