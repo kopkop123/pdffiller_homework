1b3cdb768e5a0993b71393040dcd723debab1864
denys tsymbalenko <den_cimbal@mail.ru>
2019-06-09 20:09:47 +0300

    Revert "added console.log"
    This reverts commit f61c0dbdb39148994f18e1aba0aec142e1f03527.

--- js/answers.js
+++ js/answers.js
@@ -1,8 +1,6 @@
 function saveAnswer(question, answerText) {
   const { correctOption } = question;
 
-  console.log(answerText);
-
   const isCorrect = correctOption === null ? null : correctOption === answerText;
   return { question, answerText, isCorrect }
 }
